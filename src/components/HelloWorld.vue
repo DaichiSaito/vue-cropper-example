d5aa8dbe93e208474d0bea2557c5c2dec3a84c7e
DaichiSaito <h1a_ble@yahoo.co.jp>
2019-03-30 20:25:59 +0900

    firstcommit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,92 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <input type="file" v-on:change="onFileChange()">
+    <div class="content">
+      <div class="show-info">
+        <h2>Cropper Example</h2>
+        <div class="vue-cropper-wrap">
+          <vueCropper
+            ref="cropper"
+            :img="option.img"
+            :outputSize="option.size"
+            :outputType="option.outputType"
+            :autoCrop="option.autoCrop"
+            :autoCropWidth="option.autoCropWidth"
+            :autoCropHeight="option.autoCropHeight"
+            :centerBox="option.centerBox"
+            :fixedBox="option.fixedBox"
+          ></vueCropper>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { VueCropper } from "vue-cropper";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  components: {
+    VueCropper
+  },
+  data() {
+    return {
+      option: {
+        img: "",
+        size: 1,
+        outputType: "jpeg",
+        autoCrop: true,
+        autoCropWidth: 400,
+        autoCropHeight: 400,
+        centerBox: true,
+        fixedBox: true
+      }
+    };
+  },
+  methods: {
+    onFileChange() {
+      var file = event.target.files[0];
+      if (!/\.(gif|jpg|jpeg|png|bmp|GIF|JPG|PNG)$/.test(event.target.value)) {
+        alert(".gif,jpeg,jpg,png,bmpファイルしかアップロードできません。");
+        return false;
+      }
+      var reader = new FileReader();
+      reader.onload = e => {
+        let data;
+        if (typeof e.target.result === "object") {
+          data = window.URL.createObjectURL(new Blob([e.target.result]));
+        } else {
+          data = e.target.result;
+        }
+        this.option.img = data;
+      };
+      reader.readAsArrayBuffer(file);
+    }
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<<style>
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  .content {
+    margin: auto;
+    max-width: 1200px;
+    margin-bottom: 100px;
+  }
+
+  .show-info {
+    margin-bottom: 50px;
+  }
+
+  .show-info h2 {
+    line-height: 50px;
+  }
+
+  .vue-cropper-wrap {
+    height: 500px;
+  }
 </style>
+
